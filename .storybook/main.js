bf60dbf7e22513070d8587de692e5ac6bd4d539f
let-house <play.with.letto@gmail.com>
2020-12-18 00:50:00 +0900

    [build] add storybook packages

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,6 +5,10 @@ module.exports = {
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
+    '@storybook/addon-actions',
+    '@storybook/addon-knobs',
+    '@storybook/addon-docs',
+    '@storybook/addon-a11y',
   ]
 }
